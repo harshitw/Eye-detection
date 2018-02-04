b62636960ac7aa5a264756608ee6c18c86d17ecb
harshitw <harshitwas@gmail.com>
2018-02-04 16:00:03 +0530

    Add files via upload

---
+++ eyetrack.py
@@ -0,0 +1,43 @@
+# actual eye tracking takes place here
+# combining the peices of the puzzle
+
+from learnvision.eyetracker import EyeTracker
+from learnvision import imutils
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", required = True, help = "path to the image")
+ap.add_argument("-e", "--eye", required = True, help = "path to the image")
+ap.add_argument("-v", "--video", help = "path to the (optional) video file")
+args = vars(ap.parse_args())
+
+et = EyeTracker(args["face"], args["eye"])
+
+if not args.get("video", False):
+    camera = cv2.VideoCapture(0)
+else :
+    camera = cv2.VideoCapture(args["video"])
+
+while True:
+    (grabbed, frame) = camera.read()
+
+    if args.get("video") and not grabbed:
+        break
+
+    # eye tracking starts from here
+    frame = imutils.resize(frame, width = 300)
+    # now we convert the image into grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    rects = et.track(gray)
+
+    for rect in rects :
+        cv2.rectangle(frame, (rect[0], rect[1]), (rect[2], rect[3]), (0, 255, 0), 2)
+
+    cv2.imshow("Tracking", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
